fd66d51a504711d199d97ca15778c756f3f5cd1a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-10-11 10:50:15 +0100

    statsd client added to aggregate some counters

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 from functools import reduce
+import statsd
 
 class Calc(object):
     INVALID_OPERATOR = 1
@@ -8,14 +9,20 @@ class Calc(object):
     INVALID_NUMBER_MEMBERS = 3
     INVALID_DIVISION_BY_ZERO = 4
 
+    def __init__(self, stats_client):
+        self.stats_client = stats_client
+
     def do_operation(self, operation):
+        self.stats_client.incr('operation')
         res = None
         try:
             members = [int(member) for member in operation[1:]]
         except ValueError:
+            self.stats_client.incr('operation.error.member')
             return (False, Calc.INVALID_TYPE_MEMBER)
 
         if len(members) < 2 or operation[0] == '/' and len(members) != 2:
+            self.stats_client.incr('operation.error.member_count')
             return (False, Calc.INVALID_NUMBER_MEMBERS)
 
         if operation[0] == '+':
@@ -30,23 +37,32 @@ class Calc(object):
             except ZeroDivisionError:
                 return (False, Calc.INVALID_DIVISION_BY_ZERO)
 
-        return (False, Calc.INVALID_OPERATOR) if res is None else (True, res)
+        if res is None:
+            self.stats_client.incr('operation.error.operator')
+            return (False, Calc.INVALID_OPERATOR)
+        else:
+            return (True, res)
 
     def add(self, members):
+        self.stats_client.incr('operation.sum')
         return sum(members)
 
     def sub(self, members):
+        self.stats_client.incr('operation.sub')
         return reduce(lambda x, y: x - y, members)
 
     def mult(self, members):
+        self.stats_client.incr('operation.mult')
         return reduce(lambda x, y: x*y, members)
 
     def div(self, member1, member2):
+        self.stats_client.incr('operation.div')
         return member1 / member2
 
 if __name__ == '__main__':
-    c = Calc()
-    res = c.do_operation(sys.argv[1:])
+    stats_client = statsd.StatsClient(sys.argv[1], 8125)
+    c = Calc(stats_client)
+    res = c.do_operation(sys.argv[2:])
     if res[0]:
         print(res[1])
     elif res[1] == Calc.INVALID_NUMBER_MEMBERS:
