a1eb7fea0513a9ad70ca0da78c4a7312a71b80ac
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-10-18 09:58:39 +0100

    module forgotten

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
     res = c.do_operation(sys.argv[1:])
     if res[0]:
         print(res[1])
-    elif res[1] == Calc.INVALID_NUMBER_MEMBERS:
+    elif res[1] == calc.Calc.INVALID_NUMBER_MEMBERS:
         print("Usage:")
         print(" + member1 member2 ... membern")
         print(" - member1 member2 ... membern")
         print(" x member1 member2 ... membern")
         print(" / member1 member2")
-    elif res[1] == Calc.INVALID_OPERATOR:
+    elif res[1] == calc.Calc.INVALID_OPERATOR:
         print("Invalid operator {}".format(sys.argv[1]))
-    elif res[1] == Calc.INVALID_TYPE_MEMBER:
+    elif res[1] == calc.Calc.INVALID_TYPE_MEMBER:
         print("All members must be integers")
-    elif res[1] == Calc.INVALID_DIVISION_BY_ZERO:
+    elif res[1] == calc.Calc.INVALID_DIVISION_BY_ZERO:
         print("Division by zero is not valid in this universe")
